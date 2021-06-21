9f18d6ab63dd49a740f3809470688b0ab2192bb7
Ngoc Anh <anhntnhe151378@fpt.edu.vn>
2021-06-22 01:48:57 +0700

    new function

--- src/java/controller/ManagerPostController.java
+++ src/java/controller/ManagerPostController.java
@@ -17,11 +17,13 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import javax.mail.Session;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.servlet.http.Part;
 
 /**
@@ -30,13 +32,9 @@ import javax.servlet.http.Part;
  */
 @MultipartConfig(fileSizeThreshold = 1024 * 1024 * 2, // 2MB
         maxFileSize = 1024 * 1024 * 50, // 50MB
-<<<<<<< HEAD:src/java/controller/ManagerPostController.java
-        maxRequestSize = 1024 * 1024 * 50)
+
+        maxRequestSize = 1024 * 1024 * 50, location = "C:\\Users\\HP\\OneDrive\\Desktop\\merge\\web\\assets\\images")
 public class ManagerPostController extends HttpServlet {
-=======
-        maxRequestSize = 1024 * 1024 * 50,location = "C:\\Users\\ACER\\Desktop\\SWP\\web\\assets\\images")
-public class PostDetailsController extends HttpServlet {
->>>>>>> 17c575fe38923a6965afb7addefd72078f9f4bcf:src/java/controller/PostDetailsController.java
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -62,6 +60,9 @@ public class PostDetailsController extends HttpServlet {
                 break;
             case "/manager/post/list":
                 showListPost(request, response);
+                break;
+            case "/manager/post/add":
+                showFormAddPost(request, response);
                 break;
             default:
                 break;
@@ -94,7 +95,26 @@ public class PostDetailsController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        String action = request.getServletPath();
+        switch (action) {
+            case "/manager/post/details":
+                showPostDetails(request, response);
+                break;
+            case "/manager/post/edit":
+                showFormEditPost(request, response);
+                break;
+            case "/manager/post/update":
+                updatePost(request, response);
+                break;
+            case "/manager/post/list":
+                showListPost(request, response);
+                break;
+            case "/manager/post/add":
+                addPost(request, response);
+                break;
+            default:
+                break;
+        }
     }
 
     /**
@@ -150,7 +170,7 @@ public class PostDetailsController extends HttpServlet {
         part.write("/" + File.separator + fileName);
         int pid = Integer.parseInt(request.getParameter("pid"));
         int categoryID = Integer.parseInt(request.getParameter("postCategory"));
-        String tilte = request.getParameter("title");
+        String title = request.getParameter("title");
         String description = request.getParameter("description");
         boolean featured = request.getParameter("featured").equals("true");
         boolean status = request.getParameter("status").equals("true");
@@ -160,7 +180,7 @@ public class PostDetailsController extends HttpServlet {
 
         Post p = new Post();
         p.setThumbnailLink("assets/images/" + fileName);
-        p.setTitle(tilte);
+        p.setTitle(title);
         PostCategory pc = new PostCategory();
         pc.setId(categoryID);
         p.setCategory(pc);
@@ -175,6 +195,54 @@ public class PostDetailsController extends HttpServlet {
 
         postDB.updatePost(p);
         response.sendRedirect("details?pid=" + pid);
+    }
+
+    private void showFormAddPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+        SettingDAO settingDB = new SettingDAO();
+        ArrayList<Setting> categories = settingDB.getSetting("Post category");
+        request.setAttribute("categories", categories);
+        request.getRequestDispatcher("../../view/manager/post/add.jsp").forward(request, response);
+    }
+    private void addPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Part part = request.getPart("file");
+        String fileName = extractFileName(part);
+        // refines the fileName in case it is an absolute path
+        fileName = new File(fileName).getName();
+
+        part.write("/" + File.separator + fileName);
+        int categoryID = Integer.parseInt(request.getParameter("postCategory"));
+        String title = request.getParameter("title");
+        String description = request.getParameter("description");
+        String content = request.getParameter("content");
+        boolean featured = request.getParameter("featured").equals("true");
+        boolean status = request.getParameter("status").equals("true");
+
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
+        
+        SettingDAO settingDB = new SettingDAO();
+        Setting s = settingDB.getSetting(categoryID);
+        
+        PostDAO postDB = new PostDAO();
+
+        Post p = new Post();
+        p.setThumbnailLink("assets/images/" + fileName);
+        p.setTitle(title);
+        
+        PostCategory pc = new PostCategory();
+        pc.setId(categoryID);
+        pc.setName(s.getName());
+        p.setCategory(pc);
+        
+        p.setDescription(description);
+        p.setContent(content);
+        p.setStatus(status);
+        p.setFeatured(featured);
+        p.setAuthor(user);
+        postDB.addPost(p);
+        Post newpost = postDB.getLatestPost();
+        response.sendRedirect("details?pid="+newpost.getId());
     }
 
     private String extractFileName(Part part) {
