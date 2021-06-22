b245a56f3e4260d969c9f68aace44b43b1357bb5
Ngoc Anh <anhntnhe151378@fpt.edu.vn>
2021-06-22 08:31:22 +0700

    sua db

--- DATABASE/swp.sql
+++ DATABASE/swp.sql
@@ -108,7 +108,7 @@ DROP TABLE IF EXISTS `post`;
 CREATE TABLE `post` (
   `id` int NOT NULL AUTO_INCREMENT,
   `content` text CHARACTER SET utf8 COLLATE utf8_general_ci,
-  `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `description` text CHARACTER SET utf8 COLLATE utf8_general_ci,
   `updated_date` datetime DEFAULT NULL,
   `featured` tinyint(1) DEFAULT NULL,
   `thumbnail_link` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL,
@@ -119,7 +119,7 @@ CREATE TABLE `post` (
   PRIMARY KEY (`id`) USING BTREE,
   KEY `author_id` (`author_id`) USING BTREE,
   CONSTRAINT `post_ibfk_2` FOREIGN KEY (`author_id`) REFERENCES `user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -128,7 +128,7 @@ CREATE TABLE `post` (
 
 LOCK TABLES `post` WRITE;
 /*!40000 ALTER TABLE `post` DISABLE KEYS */;
-INSERT INTO `post` VALUES (1,'Things aren\'t going well at all with mom today. She is just a limp noodle and wants to sleep all the time. I sure hope that things get better soon. \nI guess we could discuss the implications of the phrase \"meant to be.\" That is if we wanted to drown ourselves in a sea of backwardly referential semantics and other mumbo-jumbo. Maybe such a discussion would result in the determination that \"meant to be\" is exactly as meaningless a phrase as it seems to be, and that none of us is actually meant to be doing anything at all. \nBut that\'s my existential underpants underpinnings showing. It\'s the way the cookie crumbles. And now I want a cookie. It seemed like it should have been so simple. There was nothing inherently difficult with getting the project done. It was simple and straightforward enough that even a child should have been able to complete it on time, but that wasn\'t the case. The deadline had arrived and the project remained unfinished.','I\'m meant to be writing at this moment. What I mean is, I\'m meant to be writing something else at this moment.','2001-01-01 00:00:00',1,'assets/images/slider/slider.png',5,6,NULL,'Lorem Ipsum'),(2,'Content','This is important to remember. ','2001-01-01 00:00:00',1,'assets/images/slider/slider.png',5,5,NULL,'What have you noticed today?'),(3,'Content','What looked like stones in the pasture were actually rock candy','2001-01-01 00:00:00',1,'assets/images/slider/slider.png',2,7,NULL,'I am grateful for the distraction.'),(31,'Content','Description','2001-01-01 00:00:00',1,'assets/images/slider/slider.png',2,7,NULL,'Sketch Demo');
+INSERT INTO `post` VALUES (32,NULL,'Shut out from mainstream medicine, some immigrants are buying expensive, unproven Covid therapies from âwellnessâ clinics or turning to the black market.','2021-06-21 22:20:59',1,'assets/images/service/s1.jpg',5,5,1,'Desperate for Covid Care, Undocumented Immigrants Resort to Unproven Drugs'),(33,NULL,'Talk of ‘gain-of-function’ research, a muddy category at best, brings up deep questions about how scientists should study viruses and other pathogens.',NULL,1,'assets/images/service/s2.jpg',16,6,1,'Fight Over Covid’s Origins Renews Debate on Risks of Lab Work'),(34,NULL,'Organisers are to decide as soon as Monday whether to allow domestic spectators into the stadiums for the Games, which were delayed by a year due to the pandemic and now set to start in about a month. Foreign spectators have already been banned.',NULL,0,'assets/images/service/128283710.jpg',28,7,1,'Olympics venue medical officers want no spectators amid COVID-19 fears'),(35,NULL,'With summer in full swing and excessive heat waves rolling through parts of the country, taking a dip in water can be a refreshing way to cool off.',NULL,0,'assets/images/service/dd7184.png',29,8,1,'What to do to stay safe around water this summer at the pool or beach'),(36,'No Title','No Description',NULL,NULL,'assets/images/service/s3.png',5,5,NULL,'Title is None!'),(37,'Do you think you\'re living an ordinary life? You are so mistaken it\'s difficult to even explain. The mere fact that you exist makes you extraordinary. The odds of you existing are less than winning the lottery, but here you are. Are you going to let this extraordinary opportunity pass?\r\nShe had come to the conclusion that you could tell a lot about a person by their ears. The way they stuck out and the size of the earlobes could give you wonderful insights into the person. Of course, she couldn\'t scientifically prove any of this, but that didn\'t matter to her. Before anything else, she would size up the ears of the person she was talking to.\r\nIt\'s always good to bring a slower friend with you on a hike. If you happen to come across bears, the whole group doesn\'t have to worry. Only the slowest in the group do. That was the lesson they were about to learn that day.','Description','2021-06-22 00:47:44',1,'assets/images/service/Medical-logo-vector-lage.jpg',2,6,1,'222'),(39,'\"It was so great to hear from you today and it was such weird timing,\" he said. \"This is going to sound funny and a little strange, but you were in a dream I had just a couple of days ago. I\'d love to get together and tell you about it if you\'re up for a cup of coffee,\" he continued, laying the trap he\'d been planning for years.\r\nColors bounced around in her head. They mixed and threaded themselves together. Even colors that had no business being together. They were all one, yet distinctly separate at the same time. How was she going to explain this to the others?','123','2021-06-22 00:58:56',1,'assets/images/s1.jpg',2,6,1,'Project Scenario 2');
 /*!40000 ALTER TABLE `post` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -179,7 +179,7 @@ CREATE TABLE `receiver` (
   PRIMARY KEY (`id`) USING BTREE,
   KEY `FK_userid_idx` (`user_id`) USING BTREE,
   CONSTRAINT `FK_userid` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -188,7 +188,7 @@ CREATE TABLE `receiver` (
 
 LOCK TABLES `receiver` WRITE;
 /*!40000 ALTER TABLE `receiver` DISABLE KEYS */;
-INSERT INTO `receiver` VALUES (1,2,'Bo Yates',0,'0559706307','3852 Ridiculus Rd.','Fusce@loremipsum.ca'),(2,2,'John Doe',1,'012345678','6661 Neque Avenue','egestas.drerit@mauriserat.net'),(7,36,'DÆ°Æ¡ng',1,'0974484610','30','sad'),(8,-1,'Tá»ng TrÆ°á»ng Giang',1,'0977659677','19','admin@a'),(9,-1,'DÆ°Æ¡ng',1,'0977659677','30','sad@a'),(10,-1,'turuek',0,'1425678','121323123','a@a'),(11,2,'hedsafs',1,'0321234567','abcxyz','ds@fdzaws');
+INSERT INTO `receiver` VALUES (1,2,'Bo Yates',0,'0559706307','3852 Ridiculus Rd.','Fusce@loremipsum.ca'),(2,2,'John Doe',1,'012345678','6661 Neque Avenue','egestas.drerit@mauriserat.net'),(7,36,'DÆ°Æ¡ng',1,'0974484610','30','sad'),(8,-1,'Tá»ng TrÆ°á»ng Giang',1,'0977659677','19','admin@a'),(9,-1,'DÆ°Æ¡ng',1,'0977659677','30','sad@a'),(10,-1,'turuek',0,'1425678','121323123','a@a');
 /*!40000 ALTER TABLE `receiver` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -211,7 +211,7 @@ CREATE TABLE `reservation` (
   KEY `staff_id` (`staff_id`) USING BTREE,
   CONSTRAINT `reservation_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   CONSTRAINT `reservation_ibfk_2` FOREIGN KEY (`staff_id`) REFERENCES `user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -220,7 +220,7 @@ CREATE TABLE `reservation` (
 
 LOCK TABLES `reservation` WRITE;
 /*!40000 ALTER TABLE `reservation` DISABLE KEYS */;
-INSERT INTO `reservation` VALUES (2,2,'2021-06-22 22:32:39','Cancel',26,1),(36,2,'2021-06-20 00:00:00','Submited',7,1),(37,2,'2021-06-20 00:00:00','Cancel',7,1),(38,2,'2021-06-20 00:00:00','Submited',7,1),(39,2,'2021-06-20 00:00:00','Submited',7,1),(40,2,'2021-06-20 00:00:00','Submited',7,1),(41,2,'2021-06-20 00:00:00','Submited',7,1),(42,2,'2021-06-20 00:00:00','Submited',13,1),(43,2,'2021-06-20 00:00:00','Submited',13,1);
+INSERT INTO `reservation` VALUES (2,2,'2021-06-22 22:32:39','Cancel',26,1),(36,2,'2021-06-20 00:00:00','Submited',7,1),(37,2,'2021-06-20 00:00:00','Cancel',7,1);
 /*!40000 ALTER TABLE `reservation` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -248,7 +248,7 @@ CREATE TABLE `reservation_service` (
   CONSTRAINT `fk_reservation` FOREIGN KEY (`reservation_id`) REFERENCES `reservation` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   CONSTRAINT `fk_service` FOREIGN KEY (`service_id`) REFERENCES `service` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   CONSTRAINT `reservation_service_ibfk_3` FOREIGN KEY (`prescription_id`) REFERENCES `presription_detail` (`presription_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -257,7 +257,7 @@ CREATE TABLE `reservation_service` (
 
 LOCK TABLES `reservation_service` WRITE;
 /*!40000 ALTER TABLE `reservation_service` DISABLE KEYS */;
-INSERT INTO `reservation_service` VALUES (36,1,1,2,'2021-06-20',90000.00,3),(36,2,1,1,'2021-06-20',100000.00,4),(38,1,1,2,'2021-06-20',90000.00,5),(38,2,1,1,'2021-06-20',100000.00,6),(40,1,1,11,'2021-06-20',90000.00,7),(40,2,1,1,'2021-06-20',100000.00,8),(42,1,1,2,'2021-06-20',90000.00,9),(42,2,1,1,'2021-06-20',100000.00,10);
+INSERT INTO `reservation_service` VALUES (36,1,1,2,'2021-06-20',90000.00,3),(36,2,1,1,'2021-06-20',100000.00,4);
 /*!40000 ALTER TABLE `reservation_service` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -346,7 +346,7 @@ CREATE TABLE `slider` (
 
 LOCK TABLES `slider` WRITE;
 /*!40000 ALTER TABLE `slider` DISABLE KEYS */;
-INSERT INTO `slider` VALUES (1,'Chao','assets/images/slider/slider.png','#',1,'none'),(2,'Chao','assets/images/slider/slider.png','#',1,'none'),(4,'Chao','assets/images/tiktok-10x-chau-anh.jpg','#',1,'none'),(5,'Isum','assets/images/slider/slider.png','#',0,'none'),(6,'navi','assets/images/slider/slider.png','#',1,'none'),(7,'CHao','assets/images/slider/slider.png','#',0,'none'),(8,'navi','assets/images/slider/slider.png','#',0,'none'),(9,'Hue','assets/images/slider/slider.png','#',1,'none'),(10,'navi','assets/images/slider/slider.png','#',0,'none'),(11,'Ha','assets/images/slider/slider.png','#',1,'none'),(12,'Chi','assets/images/slider/slider.png','#',1,'none'),(13,'ilgnd','assets/images/slider/slider.png','#',1,'none'),(14,'navi','assets/images/slider/slider.png','#',0,'none'),(15,'navi','assets/images/slider/slider.png','#',0,'none'),(18,'navi','assets/images/UML.png','#',0,'none');
+INSERT INTO `slider` VALUES (1,'Chao1','assets/images/service/s1.jpg','#',1,'none'),(2,'Chao','assets/images/slider/slider.png','#',1,'none'),(4,'Chao','assets/images/tiktok-10x-chau-anh.jpg','#',1,'none'),(5,'Isum','assets/images/service/s2.jpg','#',0,'none'),(6,'navi','assets/images/slider/slider.png','#',1,'none'),(7,'CHao','assets/images/service/128283710.jpg','#',0,'none'),(8,'navi','assets/images/slider/slider.png','#',0,'none'),(9,'Hue','assets/images/service/dd7184.png','#',1,'none'),(10,'navi','assets/images/slider/slider.png','#',0,'none'),(11,'Ha','assets/images/service/s3.png','#',1,'none'),(12,'Chi','assets/images/slider/slider.png','#',1,'none'),(13,'ilgnd','assets/images/service/Medical-logo-vector-lage.jpg','#',1,'none'),(14,'navi','assets/images/slider/slider.png','#',0,'none'),(15,'navi','assets/images/slider/slider.png','#',0,'none'),(18,'navi','assets/images/UML.png','#',0,'none');
 /*!40000 ALTER TABLE `slider` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -380,7 +380,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (-1,'Guest','Guest','Guest',1,'0','0','0',4,1),(2,'Fusce@loremipsum.ca','Rhona','Bo Yates',0,'0559706307','3852 Ridiculus Rd.','https://i.imgur.com/EFyvmxy.png',1,1),(4,'magna@Nullatempor.co.uk','Sigourney','Guinevere Blackburn',1,'0470342636','4647 Tempor Rd.','4',1,1),(5,'cursus@Aliquamgravida.edu','Hermione','Debra Young',1,'0196303756','3259 Purus. Rd.','5',2,1),(7,'Cum.so@necmlandit.co.uk','Germane','Martina Rodgers',0,'0662288538','622-1528 Volutpat Av.','7',3,1),(10,'Integer.aliquam.adipi@vel.com','Rhonda','Linda Solis',0,'0123456789','help','10',4,0),(13,'faucibus.id@egestasadui.net','Giselle','Cally Mckinney',1,'0933925868','Ap #440-7242 Lobortis Road','13',3,1),(15,'ut.ipsum.ac@Proin.net','Mechelle','Ruth Glenn',0,'0115225427','791-3993 Diam Road','15',1,1),(16,'mollis.non.cursus@cursus.com','Gail','Sigourney Keller',0,'0271628087','289-5821 Ornare. Street','16',2,1),(17,'consequat.lectus@ipsum.edu','Althea','Jade Chang',0,'0647998539','410-9038 Semper Rd.','17',4,1),(18,'primis.in.ibus@euturpis.co.uk','Shoshana','Hollee Golden',1,'0932558198','Ap #876-8951 Enim. Rd.','18',3,1),(19,'nonummy@Maecenas.net','Keelie','Maxine Mathis',1,'0608159107','Ap #843-1727 Ac St.','19',3,1),(20,'amet.nulla.Donec@net','Leslie','Sybil Fuentes',1,'0157666445','129-2060 Adipiscing St.','20',1,1),(21,'ornare@egetmollislectus.com','Galena','Eden Grant',0,'0135037341','P.O. Box 624, 8936 In St.','21',1,1),(22,'tempus.Donec@necante.net','Ivory','May Copeland',1,'0837662391','140-2049 Amet, St.','22',1,1),(23,'nec.cursus.a@Vestibulum.edu','Bo','Mara Dalton',0,'0730490877','146-6971 Elementum, Street','23',1,1),(24,'dui.Cras.pellene@lorem.com','Imani','Cailin Patel',1,'0231918392','7950 Tristique Rd.','24',3,0),(25,'vitae.diam@dui.ca','Eden','Kai Shaw',1,'0721772746','P.O. Box 278, 9740 Quisque Street','25',3,0),(26,'egestas.henrit@merat.net','Wynne','Leilani Davis',1,'0650286200','115 Nulla Rd.','26',3,1),(27,'eu.lacus.Quisque@ligula.ca','Dara','Keiko Tucker',0,'0617291317','Ap #549-5875 Posuere St.','27',1,1),(28,'Nam.ac@interdumSed.com','Caryn','Madeline Mckee',0,'0915978706','3222 Nec, Rd.','28',2,1),(29,'molestie@Proin.edu','Lillith','Kelsie Hogan',1,'0655287977','704-102 Magna. Road','29',2,1),(30,'et@ultriciesadigenim.edu','Angela','Meredith Newton',0,'0436457125','Ap #724-7504 Eu St.','30',4,1),(32,'giangtong09@gmail.com','huepham1905','Chao',1,'0974484610','19',NULL,4,1),(33,'admin','admin','Chaos',0,'0974484610','19',NULL,4,1),(34,'sssss','admin','asd',1,'0974484610','19',NULL,4,1),(35,'user','useer','DÆ°Æ¡ng',1,'ad','30',NULL,4,0),(36,'sad','sad','DÆ°Æ¡ng',1,'0974484610','30',NULL,4,1),(37,'ysysuuueii@dwd','null0000000000','Gae',1,'13253562','2453245','None',4,1),(38,'qwdqw@eee','null0000000000','Trrre',0,'12354654','12123123','None',4,0),(39,'ysysuu1eeueii@dwd','CRS0000000000','qweqwe',1,'123123','12323','None',1,1),(40,'test@test','CRS294324825','123123',1,'342141','1324','None',4,1);
+INSERT INTO `user` VALUES (-1,'Guest','Guest','Guest',1,'0','0','0',4,1),(2,'Fusce@loremipsum.ca','Rhona','Bo Yates',0,'0559706307','3852 Ridiculus Rd.','https://i.imgur.com/EFyvmxy.png',1,1),(4,'magna@Nullatempor.co.uk','Sigourney','Guinevere Blackburn',1,'0470342636','4647 Tempor Rd.','4',1,1),(5,'cursus@Aliquamgravida.edu','Hermione','Debra Young',1,'0196303756','3259 Purus. Rd.','5',2,1),(7,'Cum.so@necmlandit.co.uk','Germane','Martina Rodgers',0,'0662288538','622-1528 Volutpat Av.','7',3,1),(10,'Integer.aliquam.adipi@vel.com','Rhonda','Linda Solis',0,'0321248916','6661 Neque Avenue','10',4,1),(13,'faucibus.id@egestasadui.net','Giselle','Cally Mckinney',1,'0933925868','Ap #440-7242 Lobortis Road','13',3,1),(15,'ut.ipsum.ac@Proin.net','Mechelle','Ruth Glenn',0,'0115225427','791-3993 Diam Road','15',1,1),(16,'mollis.non.cursus@cursus.com','Gail','Sigourney Keller',0,'0271628087','289-5821 Ornare. Street','16',2,1),(17,'consequat.lectus@ipsum.edu','Althea','Jade Chang',0,'0647998539','410-9038 Semper Rd.','17',4,1),(18,'primis.in.ibus@euturpis.co.uk','Shoshana','Hollee Golden',1,'0932558198','Ap #876-8951 Enim. Rd.','18',3,1),(19,'nonummy@Maecenas.net','Keelie','Maxine Mathis',1,'0608159107','Ap #843-1727 Ac St.','19',3,1),(20,'amet.nulla.Donec@net','Leslie','Sybil Fuentes',1,'0157666445','129-2060 Adipiscing St.','20',1,1),(21,'ornare@egetmollislectus.com','Galena','Eden Grant',0,'0135037341','P.O. Box 624, 8936 In St.','21',1,1),(22,'tempus.Donec@necante.net','Ivory','May Copeland',1,'0837662391','140-2049 Amet, St.','22',1,1),(23,'nec.cursus.a@Vestibulum.edu','Bo','Mara Dalton',0,'0730490877','146-6971 Elementum, Street','23',1,1),(24,'dui.Cras.pellene@lorem.com','Imani','Cailin Patel',1,'0231918392','7950 Tristique Rd.','24',3,0),(25,'vitae.diam@dui.ca','Eden','Kai Shaw',1,'0721772746','P.O. Box 278, 9740 Quisque Street','25',3,0),(26,'egestas.henrit@merat.net','Wynne','Leilani Davis',1,'0650286200','115 Nulla Rd.','26',3,1),(27,'eu.lacus.Quisque@ligula.ca','Dara','Keiko Tucker',0,'0617291317','Ap #549-5875 Posuere St.','27',1,1),(28,'Nam.ac@interdumSed.com','Caryn','Madeline Mckee',0,'0915978706','3222 Nec, Rd.','28',2,1),(29,'molestie@Proin.edu','Lillith','Kelsie Hogan',1,'0655287977','704-102 Magna. Road','29',2,1),(30,'et@ultriciesadigenim.edu','Angela','Meredith Newton',0,'0436457125','Ap #724-7504 Eu St.','30',4,1),(32,'giangtong09@gmail.com','huepham1905','Chao',1,'0974484610','19',NULL,4,1),(33,'admin','admin','Chaos',0,'0974484610','19',NULL,4,1),(34,'sssss','admin','asd',1,'0974484610','19',NULL,4,1),(35,'user','useer','DÆ°Æ¡ng',1,'ad','30',NULL,4,0),(36,'sad','sad','DÆ°Æ¡ng',1,'0974484610','30',NULL,4,1),(37,'ysysuuueii@dwd','null0000000000','Gae',1,'13253562','2453245','None',4,1),(38,'qwdqw@eee','null0000000000','Trrre',0,'12354654','12123123','None',4,0),(39,'ysysuu1eeueii@dwd','CRS0000000000','qweqwe',1,'123123','12323','None',1,1),(40,'test@test','CRS294324825','123123',1,'342141','1324','None',4,1);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -415,7 +415,6 @@ CREATE TABLE `user_history` (
 
 LOCK TABLES `user_history` WRITE;
 /*!40000 ALTER TABLE `user_history` DISABLE KEYS */;
-INSERT INTO `user_history` VALUES (10,'Integer.aliquam.adipi@vel.com','Linda Solis',1,'0321248916','help',0,2,'2021-06-20 21:39:31',4),(10,'Integer.aliquam.adipi@vel.com','Linda Solis',0,'0321248916','help',0,2,'2021-06-21 09:48:11',4),(10,'Integer.aliquam.adipi@vel.com','Linda Solis',1,'0321248916','help',0,2,'2021-06-21 09:51:10',4),(10,'Integer.aliquam.adipi@vel.com','Linda Solis',0,'0321248916','help',0,2,'2021-06-21 10:22:29',4),(10,'Integer.aliquam.adipi@vel.com','Linda Solis',0,'0123456789','help',0,2,'2021-06-21 10:25:06',4);
 /*!40000 ALTER TABLE `user_history` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -428,4 +427,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-06-21 18:20:29
+-- Dump completed on 2021-06-22  8:30:49
--- src/java/controller/ManagerPostController.java
+++ src/java/controller/ManagerPostController.java
@@ -138,7 +138,7 @@ public class ManagerPostController extends HttpServlet {
         request.setAttribute("authors", authors);
         request.setAttribute("categories", categories);
         request.setAttribute("posts", posts);
-        request.getRequestDispatcher("../../view/manager/post/list.jsp").forward(request, response);
+        request.getRequestDispatcher("../../view/manager/post/list2.jsp").forward(request, response);
     }
 
     private void showPostDetails(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
