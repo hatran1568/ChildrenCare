81fdcf085788a74efaf6624d977f164fe249019d
Tran Thi Nguyet Ha <hattnhe153222@fpt.edu.vn>
2021-07-12 21:49:38 +0700

    fix jsp

--- src/java/dao/BaseDAO.java
+++ src/java/dao/BaseDAO.java
@@ -17,7 +17,7 @@ public class BaseDAO {
     public BaseDAO()
     {
         try {
-            String user = "root";
+            String user = "sa";
             String pass = "123456";
             String url = "jdbc:mysql://localhost:3306/swp?autoReconnect=true&useSSL=false&allowPublicKeyRetrieval=true";
             Class.forName("com.mysql.jdbc.Driver");
